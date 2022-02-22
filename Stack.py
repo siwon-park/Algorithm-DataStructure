4d1c1ea61247ae4f412be45a0da1a5315f1754a5
박시원 <zow777@naver.com>
2022-02-22 19:50:34 +0900

    Update Stack.py
    peek 메서드 추가, size attribute 추가, 스택 마지막 요소 수정(last -> top)

--- Stack.py
+++ Stack.py
@@ -9,22 +9,32 @@ class Node:
 # 스택 클래스 선언
 class Stack:
     def __init__(self):
-        self.last = None
+        self.top = None
+        self.size = 0
 
     def push(self, item):
-        self.last = Node(item, self.last) # self.last.next에 self.last(=None을 넣음)
-        # self.last.next = None임
-    def pop(self):
-        item = self.last.item
-        self.last = self.last.next # None
+        self.top = Node(item, self.top) # self.top.next에 self.top(=None을 넣음), self.top.next = None임
+        self.size += 1
+
+    def pop(self): # pop은 스택의 마지막 요소를 뽑은 다음 반환
+        if self.size == 0: # 스택이 비어있으면 pop을 할 수 없음
+            return
+        item = self.top.item # item 변수에 스택의 마지막 요소의 값인 self.top.item을 할당하고
+        self.top = self.top.next # 스택의 마지막 요소를 self.top.next(=None)으로 바꾼다(=요소를 뽑아서 없앰)
+        self.size -= 1
         return item
 
     def isEmpty(self):
-        return self.last is None
+        return self.top is None
+
+    def peek(self): # peek은 스택의 마지막 요소를 뽑지는 않고 반환만 함
+        return self.top.item
 
 stack = Stack()
 stack.push(4)
 stack.push(5)
+print(stack.peek()) # 5 (요소를 뽑지는 않음)
+print(stack.size) # 2
 print(stack.isEmpty()) # False
 print(stack.pop()) # 5
 print(stack.pop()) # 4
