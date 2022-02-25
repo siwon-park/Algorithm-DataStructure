b2a8e323a47d2821a0e6e2affb64b17af0ea9c28
박시원 <zow777@naver.com>
2022-02-25 22:32:02 +0900

    Update Linear_Queue.py

--- Linear_Queue.py
+++ Linear_Queue.py
@@ -1,7 +1,7 @@
 # 선형 큐 구현
 class LinearQueue:
     def __init__(self, max_size):
-        self.q = [0] * max_size
+        self.q = [0] * max_size # [None] * max_size # 삽입되는 요소가 0일 수도 있으니 None으로 초기화
         self.size = max_size
         self.front = -1
         self.rear = -1
@@ -16,14 +16,14 @@ class LinearQueue:
         if LinearQueue.isFull(self):
             print("Can't Append Item Because Of Queue OverFlow")
             return
-        self.rear = self.rear + 1
+        self.rear = self.rear + 1 # rear의 default가 -1이니 1증가시킨 다음 삽입을 시작함
         self.q[self.rear] = item
 
     def deQueue(self):
         if LinearQueue.isEmpty(self):
             print("Can't Pop From Empty Queue")
             return
-        self.front = self.front + 1
+        self.front = self.front + 1 # front의 default가 -1이니 1증가시킨 다음 삭제
         return self.q[self.front]
 
 q = LinearQueue(4)
