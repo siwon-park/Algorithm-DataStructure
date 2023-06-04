9dbae978a739aca463552e4a7fab26d8f55b1ccb
siwon-park <zow777@naver.com>
2023-06-04 12:25:57 +0900

    docs: 세그먼트 트리 업데이트 연산 코드 업데이트

--- 00_refrence_code/Java/04_Tree/SegmentTree.java
+++ 00_refrence_code/Java/04_Tree/SegmentTree.java
@@ -14,11 +14,22 @@ class SegmentTree {
         return tree[n] = init(s, mid, n * 2) + init(mid + 1, e, n * 2 + 1);
     }
 
+    // 업데이트 연산1 -> 업데이트 연산2와 동일함
     int update(int s, int e, int n, int idx, int val) {
         if (idx < s || e < idx) return tree[n];
         if (idx == s && e == idx) return tree[n] = val;
         int mid = (s + e) / 2;
         return tree[n] = update(s, mid, n * 2, idx, val) + update(mid + 1, e, n * 2 + 1, idx, val);
+    }
+
+    // 업데이트 연산2 -> 업데이트 연산1과 동일함
+    void update2(int s, int e, int n, int idx, int val) {
+        if (idx < s || e < idx) return;
+        tree[n] += val; // idx를 포함하는 모든 구간에 대해서 val값을 업데이트 하므로 업데이트 연산1과 결국 같음
+        if (s == e) return;
+        int mid = (s + e) / 2;
+        update2(s, mid, n * 2, idx, val);
+        update2(mid + 1, e, n * 2 + 1, idx, val);
     }
 
     int sum(int s, int e, int n, int l, int r) {
