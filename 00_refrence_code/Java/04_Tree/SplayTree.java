8b59c34dc2153ac190c3cd1bcdf3f05d267442af
siwon-park <zow777@naver.com>
2023-12-24 14:00:58 +0900

    etc: 스플레이 트리 insert, find, delete 메서드 구현

--- 00_refrence_code/Java/04_Tree/SplayTree.java
+++ 00_refrence_code/Java/04_Tree/SplayTree.java
@@ -3,7 +3,7 @@ class SplayTree {
     Node root;
     SplayTree() {}
     
-    // rotate: 특정 노드를 자신의 부모 위치로 올리는 함수
+    // rotate; 특정 노드를 자신의 부모 위치로 올리는 함수
     void rotate(Node x) {
         Node p = x.parent; // x의 부모 노드
         // Node b = null;
@@ -37,7 +37,7 @@ class SplayTree {
 
     }
 
-    // splay: 특정 노드를 루트 노드 위치로 옮기는 함수
+    // splay; 특정 노드를 루트 노드 위치로 옮기는 함수
     void splay(Node x) {
         while (x.parent != null) { // x가 루트라면 종료
             Node p = x.parent; // x의 부모
@@ -59,14 +59,99 @@ class SplayTree {
         }
     }
 
-    // insert: 삽입
+    // insert; 삽입 -> 마지막에 삽입한 노드를 splay연산을 해줌
     void insert(int key) {
+        Node newNode = new Node(key);
+        if (root == null) { // 루트가 비었을 경우
+            root = newNode;
+            return;
+        }
 
+        Node cur = root;
+        Node parent = null;
+        while (cur != null) {
+            parent = cur;
+            if (parent.key == key) { // 중복일 경우 return
+                return;
+            }
+            if (key < cur.key) { // 삽입하려는 key가 현재 노드보다 작으면 왼쪽으로 이동
+                cur = cur.left;
+            } else { // key가 현재 노드보다 크다면 오른쪽으로 이동
+                cur = cur.right;
+            }
+        }
+        // current가 null이 되는 순간 while문을 빠져나옴; current의 parent는 null이 아님.
+        newNode.parent = parent;
+        if (key < newNode.parent.key) {
+            newNode.parent.left = newNode;
+        } else {
+            newNode.parent.right = newNode;
+        }
+
+        splay(newNode);
     }
 
-    // find: 찾기
+    // find; 찾기 -> 마지막에 검색한 노드를 splay 연산을 해줌
     boolean find(int key) {
-        return false;
+        if (root == null) { // 루트가 비었으면 false 반환
+            return false;
+        }
+        Node cur = root;
+        while (cur != null) {
+            if (key == cur.key) break; // 탐색 성공
+            if (key < cur.key) { // 찾고자 하는 key가 현재 노드의 key보다 작으면 왼쪽으로 탐색
+                if (cur.left == null) { // 현재 노드의 왼쪽 자식 노드가 null이면 탐색 종료
+                    break;
+                }
+                cur = cur.left;
+            } else { // 찾고자 하는 key가 현재 노드의 key보다 크면 오른쪽으로 탐색
+                if (cur.right == null) { // 현재 노드의 오른쪽 자식 노드가 null이면 탐색 종료
+                    break;
+                }
+                cur = cur.right;
+            }
+        }
+        
+        splay(cur); // 마지막에 탐색한 노드를 루트로 이동
+        return key == cur.key;
+    }
+
+    // delete; 특정 key값을 가진 노드를 삭제 -> 삭제할 노드를 splay한 다음에 삭제.
+    // 단 자식이 1개 이하면 바로 삭제하지만, 자식이 2개라면 서브 트리를 합치는 과정이 필요함
+    void delete(int key) {
+        if (!find(key)) { // 찾고자하는 키가 없으면 탐색 종료
+            return;
+        }
+        // find 메서드에서 이미 splay연산을 한 상태이기 때문에 별도의 splay 연산이 필요 없음
+        Node p = root;
+        if (p.left != null && p.right != null) { // 왼쪽, 오른쪽 서브 트리가 모두 존재하는 경우
+            // 왼쪽 서브 트리를 루트로 만듦
+            root = p.left;
+            root.parent = null;
+
+            // 오른쪽 서브 트리를 왼쪽 서브 트리 아래에 삽입(왼쪽 서브 트리의 오른쪽에 삽입)
+            Node x = root;
+            while (x.right != null) {
+                x = x.right;
+            }
+            x.right = p.right;
+            p.right.parent = x;
+            return;
+        }
+        if (p.left != null) { // 왼쪽 자식만 있는 경우, 왼쪽 자식을 루트로 만들고 return
+            root = p.left;
+            root.parent = null;
+            return;
+        }
+        if (p.right != null) { // 오른쪽 자식만 있는 경우, 오른쪽 자식을 루트로 만들고 return
+            root = p.right;
+            root.parent = null;
+            return;
+        }
+        
+        // 자신(루트)밖에 없을 경우 루트를 삭제
+        root = null;
+        return;
     }
 
 }
