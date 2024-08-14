7c09378afdd06696ef080fd7c35fc483065c932a
siwon-park <zow777@naver.com>
2024-08-14 21:33:12 +0900

    fix: 펜윅 트리 사이즈 및 설명 수정

--- 00_refrence_code/Java/04_Tree/FenwickTree.java
+++ 00_refrence_code/Java/04_Tree/FenwickTree.java
@@ -8,14 +8,13 @@ class FenwickTree {
 	int treeSize;
 	
 	FenwickTree(int n) {
-		// 1부터 n 포함이기 때문에 n + 1
-		this.tree = new long[n + 1];
-		this.treeSize = n + 1;
+		this.tree = new long[n + 1]; // 1부터 n 포함이기 때문에 n + 1
+		this.treeSize = n; // 트리의 크기는 n
 	}
 	
 	// 업데이트할 값과 현재 값의 차이인 v만큼을 대상 구간에 갱신시켜주고, 배열의 해당 위치 값을 업데이트한 결과값으로 변경
 	void update(int i, long v) {
-		while (i < treeSize) { // treeSize가 n + 1이기 때문에 n까지 계산하기 위해 n + 1보다 작을 동안 반복
+		while (i <= treeSize) { // treeSize 이하일 만큼 반복
 			tree[i] += v;
 			i += (i & -i);
 		}
